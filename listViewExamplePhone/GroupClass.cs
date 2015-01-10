d90cd2b1a0289c06aa40fe72fdf449bae758bf51
shivamh71 <shivamh71@gmail.com>
2015-01-10 10:17:11 +0530

    first commit

---
+++ listViewExamplePhone/GroupClass.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace listViewExamplePhone
+{
+    public class GroupClass
+    {
+        public int GroupId { get; set; }
+        public string GroupName { get; set; }
+        public List<string> members { get; set; }
+
+        public GroupClass(int GroupId, string gname)
+        {
+            this.GroupId = GroupId;
+            this.GroupName = gname;
+            members = new List<string>();
+        }
+
+        public void AddMember(string name)
+        {
+            this.members.Add(name);
+        }
+    }
+}
+
---
+++ listViewExamplePhone/MainPage.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=391641
+
+namespace listViewExamplePhone
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page 
+    {
+        public ObservableCollection<GroupClass> Groups { get; set; }
+
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+            Groups = new ObservableCollection<GroupClass>();
+            GroupItems.DataContext = Groups;
+            this.NavigationCacheMode = NavigationCacheMode.Required;
+            
+        }
+
+        /// <summary>
+        /// Invoked when this page is about to be displayed in a Frame.
+        /// </summary>
+        /// <param name="e">Event data that describes how this page was reached.
+        /// This parameter is typically used to configure the page.</param>
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            // TODO: Prepare page for display here.
+
+            // TODO: If your application contains multiple pages, ensure that you are
+            // handling the hardware Back button by registering for the
+            // Windows.Phone.UI.Input.HardwareButtons.BackPressed event.
+            // If you are using the NavigationHelper provided by some templates,
+            // this event is handled for you.
+            
+        }
+
+        private void AddGroup_Click(object sender, RoutedEventArgs e)
+        {
+            GroupClass nGroup = new GroupClass(12, "My family");
+            Groups.Add(nGroup);
+        }
+
+        private void GroupClicked(object sender, SelectionChangedEventArgs e)
+        {
+            Debug.WriteLine("Selected: {0}", e.AddedItems[0]);
+        }
+    }
+}
